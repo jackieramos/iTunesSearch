5a0bc7f2ec0ee26f4cbcbfbf68a78011839e5d41
Jackielynn Ramos <jackielynn.ramos@gmail.com>
2021-07-12 14:34:02 +0800

    Add LastVisitedView

---
+++ KumuSkillTest/Core/AppUserDefaults.swift
@@ -0,0 +1,9 @@
+//
+//  UserDefaults.swift
+//  KumuSkillTest
+//
+//  Created by Jackie Ramos on 7/12/21.
+//
+
+import Foundation
+
---
+++ KumuSkillTest/Views/UIViews/LastVisitedView/LastVisitedView.swift
@@ -0,0 +1,17 @@
+//
+//  LastVisitedView.swift
+//  KumuSkillTest
+//
+//  Created by Jackie Ramos on 7/12/21.
+//
+
+import UIKit
+
+class LastVisitedView: UIView {
+
+    @IBOutlet weak var lastVisitedLabel: UILabel!
+    
+    func bind(_ date: Date) {
+        
+    }
+}
